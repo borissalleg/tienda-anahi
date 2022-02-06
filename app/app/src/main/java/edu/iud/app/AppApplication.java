bcbb6cc927dcb0fc6a76215b1cb70f16e85cf392
Boris Salleg <borissalleg@gmail.com>
2022-02-06 15:06:07 -0500

    configuracion de CORS

--- app/app/src/main/java/edu/iud/app/AppApplication.java
+++ app/app/src/main/java/edu/iud/app/AppApplication.java
@@ -2,6 +2,8 @@ package edu.iud.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AppApplication {
@@ -9,6 +11,15 @@ public class AppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 		System.out.println("App cargada...");
+	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("https://borisfrontreact.herokuapp.com/");
+			}
+		};
 	}
 
 }
